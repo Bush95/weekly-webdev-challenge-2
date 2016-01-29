f0d84b217241535277aecc52f29205218b9f8c7b
Bush95 <adi139597@gmail.com>
2016-01-29 22:51:18 +0100

    adding finished project

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 new WOW().init();
 
+$('a').click(function () {
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+});
+
+$("#header-scroll-button").click(function () {
+    var scroll_btn = this;
+
+    $(scroll_btn).addClass('rotated');
+    setTimeout(function () {
+        $(scroll_btn).removeClass('rotated');
+    }, 500);
+});
+
+
+$('#gallery img').hover(function(){
+    $('#gallery img').not($(this)).css({
+       '-webkit-filter' : 'grayscale(80%)',
+       'filter' : 'grayscale(80%)'
+    });
+},
+function(){
+    $('#gallery img').not($(this)).css({
+       '-webkit-filter' : 'grayscale(0%)',
+       'filter' : 'grayscale(0%)'
+    });
+});
+
+
