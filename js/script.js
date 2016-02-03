b48a7167bf8a122afa05594f91ff2e8c81c02c0d
Bush95 <adi139597@gmail.com>
2016-02-03 13:34:40 +0100

    Less code, added fixed navbar, final version

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 new WOW().init();
 
+
 $('a').click(function () {
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top - $(".navbar-header").height()
     }, 500);
     return false;
 });
 
 $("#header-scroll-button").click(function () {
     var scroll_btn = this;
-
     $(scroll_btn).addClass('rotated');
     setTimeout(function () {
         $(scroll_btn).removeClass('rotated');
     }, 500);
 });
-
 
 $('#gallery img').hover(function(){
     $('#gallery img').not($(this)).css({
