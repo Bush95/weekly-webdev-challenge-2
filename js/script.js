2442166ed01f6329e6c973d098ad63d7e6577228
Bush95 <adi139597@gmail.com>
2016-02-12 15:27:58 +0100

    navbar fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$('a').click(function () {
+$('a').click(function (e) {
+    e.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - $(".navbar-header").height()
     }, 500);
-    return false;
 });
 
 $("#header-scroll-button").click(function () {
@@ -12,18 +12,3 @@ $("#header-scroll-button").click(function () {
         $(scroll_btn).removeClass('rotated');
     }, 500);
 });
-
-$('#gallery img').hover(function(){
-    $('#gallery img').not($(this)).css({
-       '-webkit-filter' : 'grayscale(80%)',
-       'filter' : 'grayscale(80%)'
-    });
-},
-function(){
-    $('#gallery img').not($(this)).css({
-       '-webkit-filter' : 'grayscale(0%)',
-       'filter' : 'grayscale(0%)'
-    });
-});
-
-
