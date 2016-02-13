4d16e758cbe2865b8d65b7433cf1bd044b29bfea
Bush95 <adi139597@gmail.com>
2016-02-13 10:36:58 +0100

    now navbar is really working correctly

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-$('a').click(function (e) {
+$('#nav-collapsed a, #header-scroll-button').click(function (e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - $(".navbar-header").height()
     }, 500);
-});
+
+    $('#nav-collapsed').animate({
+       height : 0
+    }, 200);
+    
+    $('#nav-collapsed').promise().done(function(){
+        $('#nav-collapsed').removeClass('collapse').removeClass('in').addClass('collapse').attr('aria-expanded',false);
+        $('.navbar-toggle').addClass('collapsed').attr('aria-expanded',false);
+    });
+    
+     });
 
 $("#header-scroll-button").click(function () {
     var scroll_btn = this;
